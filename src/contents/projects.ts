cf52e7812b49203928280f79c9581035df0c1a16
goeyDev <chgoey@outlook.com>
2025-07-01 14:21:18 +0800

    add todo Task

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -42,14 +42,21 @@ export const projects: Project[] = [
         githubLink: 'https://github.com',
         demoLink: 'https://ecom1-umber.vercel.app/',
         image: '/ecom1.svg',
-      },
-          {
+      },{
         title: 'Sleep Tracker',
         description: 'Track your sleep, improve your health, and wake up feeling refreshed with SleepTracker.',
         technologies: ['React', 'Nextjs','Drizzle-orm', 'Tailwind CSS'],
         githubLink: 'https://github.com',
         demoLink: 'https://sleep-tracker-nine.vercel.app/',
         image: '/sleepTracker.svg',
+      },
+       {
+        title: 'Todo Task',
+        description: 'Track Daily Todo Taks status.',
+        technologies: ['React', 'Nextjs','Drizzle-orm', 'Tailwind CSS'],
+        githubLink: 'https://github.com',
+        demoLink: 'https://todo-task-ten-navy.vercel.app/',
+        image: '/todotask.svg',
       },
      
   ];
