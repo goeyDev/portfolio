a6ceb330e2caee62b807865c2ef6336fdda77562
goeyDev <chgoey@outloog.com>
2025-07-11 22:59:53 +0800

    upgrade auth function

--- src/contents/projects.ts
+++ src/contents/projects.ts
@@ -1,62 +1,65 @@
 import { Project } from "@/types/index";
 
-
 export const projects: Project[] = [
-    {
-        title: 'E-commerce Platform',
-        description: 'A full-stack e-commerce platform built with Next.js, TypeScript, and Tailwind CSS.',
-        technologies: ['Next.js', 'TypeScript', 'Tailwind CSS', 'Stripe'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://ped-ecom-goeys-projects.vercel.app/',
-        image: '/ecommerce.svg',
-      },
-      {
-        title: 'Portfolio Website',
-        description: 'My personal portfolio website showcasing my projects and skills.',
-        technologies: ['Next.js', 'Tailwind CSS', 'Framer Motion'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://portfolio-umber-zeta-fwp2uibjjf.vercel.app',
-        image: '/portfolio.svg',
-      },
-      {
-        title: 'Computer Repair Shop',
-        description: 'A blogging platform with user authentication and markdown support.',
-        technologies: ['React', 'Nextjs','Drizzle-orm', 'Tailwind CSS'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://cr-shop-eight.vercel.app/tickets',
-        image: '/repairshop.svg',
-      },
-      {
-        title: 'IncomeExpenses',
-        description: 'A personal finance tracker to manage expenses and income.',
-        technologies: ['React', 'Nextjs','Drizzle-orm', 'Tailwind CSS'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://income-expense-nextjs-sigma.vercel.app/',
-        image: '/incexp.svg',
-      },
-  
-      {
-        title: 'Vehicle History',
-        description: 'Maintenance and Repair History of Vehicle .',
-        technologies: ['React', 'Nextjs','Drizzle-orm', 'Tailwind CSS'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://ecom1-umber.vercel.app/',
-        image: '/ecom1.svg',
-      },{
-        title: 'Sleep Tracker',
-        description: 'Track your sleep, improve your health, and wake up feeling refreshed with SleepTracker.',
-        technologies: ['React', 'Nextjs','Drizzle-orm', 'Tailwind CSS'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://sleep-tracker-nine.vercel.app/',
-        image: '/sleepTracker.svg',
-      },
-       {
-        title: 'Todo Task',
-        description: 'Track Daily Todo Taks status.',
-        technologies: ['React', 'Nextjs','Drizzle-orm', 'Tailwind CSS'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://todo-task-ten-navy.vercel.app/',
-        image: '/todotask.svg',
-      },
-     
-  ];
+  {
+    title: "E-commerce Platform",
+    description:
+      "A full-stack e-commerce platform built with Next.js, TypeScript, and Tailwind CSS.",
+    technologies: ["Next.js", "TypeScript", "Tailwind CSS", "Stripe"],
+    githubLink: "https://github.com",
+    demoLink: "https://ecommerce-store-pearl-chi.vercel.app/",
+    image: "/ecommerce.svg",
+  },
+  {
+    title: "Portfolio Website",
+    description:
+      "My personal portfolio website showcasing my projects and skills.",
+    technologies: ["Next.js", "Tailwind CSS", "Framer Motion"],
+    githubLink: "https://github.com",
+    demoLink: "https://portfolio-umber-zeta-fwp2uibjjf.vercel.app",
+    image: "/portfolio.svg",
+  },
+  {
+    title: "Computer Repair Shop",
+    description:
+      "A blogging platform with user authentication and markdown support.",
+    technologies: ["React", "Nextjs", "Drizzle-orm", "Tailwind CSS"],
+    githubLink: "https://github.com",
+    demoLink: "https://cr-shop-eight.vercel.app/tickets",
+    image: "/repairshop.svg",
+  },
+  {
+    title: "IncomeExpenses",
+    description: "A personal finance tracker to manage expenses and income.",
+    technologies: ["React", "Nextjs", "Drizzle-orm", "Tailwind CSS"],
+    githubLink: "https://github.com",
+    demoLink: "https://income-expense-nextjs-sigma.vercel.app/",
+    image: "/incexp.svg",
+  },
+
+  {
+    title: "Vehicle History",
+    description: "Maintenance and Repair History of Vehicle .",
+    technologies: ["React", "Nextjs", "Drizzle-orm", "Tailwind CSS"],
+    githubLink: "https://github.com",
+    demoLink: "https://ecom1-umber.vercel.app/",
+    image: "/ecom1.svg",
+  },
+  {
+    title: "Sleep Tracker",
+    description:
+      "Track your sleep, improve your health, and wake up feeling refreshed with SleepTracker.",
+    technologies: ["React", "Nextjs", "Drizzle-orm", "Tailwind CSS"],
+    githubLink: "https://github.com",
+    demoLink: "https://sleep-tracker-nine.vercel.app/",
+    image: "/sleepTracker.svg",
+  },
+  {
+    title: "Todo Task",
+    description: "Track Daily Todo Taks status.",
+    technologies: ["React", "Nextjs", "Drizzle-orm", "Tailwind CSS"],
+    githubLink: "https://github.com",
+    demoLink: "https://todo-task-ten-navy.vercel.app/",
+    image: "/todotask.svg",
+  },
+];
